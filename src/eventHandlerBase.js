8b2bcc72dbe05091aa5e8342a2f071e8bc460939
Raif Harik <reharik@gmail.com>
2015-10-04 19:15:43 -0500

    more of same

--- src/eventHandlerBase.js
+++ src/eventHandlerBase.js
@@ -29,6 +29,7 @@ module.exports = function(eventstore, readstorerepository, eventmodels, logger) 
                 try{
                     console.log(gesEvent.data);
                     console.log(gesEvent.metadata.continuationId);
+                    console.log(this[gesEvent.eventName]);
                     console.log(this[gesEvent.eventName](gesEvent.data, gesEvent.metadata.continuationId));
                 }catch(ex){
                     console.log(ex);
