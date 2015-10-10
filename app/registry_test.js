c3a4b3593886a5fb6e9c427ba3d884a1a4fe3ee0
Raif Harik <reharik@gmail.com>
2015-10-10 18:15:15 -0500

    rearrainging

--- app/registry_test.js
+++ app/registry_test.js
@@ -2,22 +2,19 @@
  * Created by parallels on 9/3/15.
  */
 var dagon = require('dagon');
+var path = require('path');
 
 module.exports = function(_options) {
     var options = _options || {};
     var container = dagon(options.dagon);
-    var instance = new container(x=>
-        x.pathToRoot(__dirname)
-            .requireDirectoryRecursively('./src')
-            .requireDirectoryRecursively('./tests/unitTests/mocks')
+    return new container(x=>
+        x.pathToRoot(path.join(__dirname,'..'))
+            .requireDirectoryRecursively('./app/src')
+            .requireDirectoryRecursively('./app/tests/unitTests/mocks')
             .for('bluebird').renameTo('Promise')
             .for('corelogger').renameTo('logger').instantiate(i=>i.asFunc().withParameters(options.logger || {}))
             .for('eventmodels').instantiate(i=>i.asFunc())
-            .for('readstorerepository').require('./tests/unitTests/mocks/readStoreRepositoryMock')
-            .for('eventstore').require('./tests/unitTests/mocks/eventStoreMock')
+            .for('readstorerepository').require('./app/tests/unitTests/mocks/readStoreRepositoryMock')
+            .for('eventstore').require('./app/tests/unitTests/mocks/eventStoreMock')
             .complete());
-
-    console.log(instance.whatDoIHave());
-    return instance;
-
 };
