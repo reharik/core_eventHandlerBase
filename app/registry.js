06a47678ba299538d7b914e6c0f1f473a2dac2f8
Raif Harik <reharik@gmail.com>
2015-12-29 09:58:40 -0600

    updating to newVersion of dagon

--- app/registry.js
+++ app/registry.js
@@ -8,16 +8,9 @@ var path = require('path');
 
 module.exports = function(_options) {
     var options = _options || {};
-    var container = dagon(options.dagon);
-    return new container(x=>
+    var registry = dagon(options.dagon).registry;
+    return registry(x=>
         x.pathToRoot(path.join(__dirname,'..'))
             .requireDirectoryRecursively('./app/src')
-            .for('bluebird').renameTo('Promise')
-            .for('corelogger').renameTo('logger').instantiate(i=>i.asFunc().withParameters(options.logger || {}))
-            .for('eventmodels').instantiate(i=>i.asFunc())
-            .for('eventstore').instantiate(i=>i.asFunc().withParameters(options || {}))
-            .for('readstorerepository').instantiate(i=>i.asFunc().withParameters(options || {}))
-            .for('ramda').renameTo('R')
-            .for('ramdafantasy').renameTo('_fantasy')
             .complete());
 };
